178bf9f224bec02eb056cab697ce70ddf432bc5e
--replace-all <jonathonsmcclendon@gmail.com>
2024-05-20 11:19:25 -0500

    init mmo
    no multiplayer yet will implement database conections for updates on level, x/y cords, and state

---
+++ src/Groups/Bullets.js
@@ -0,0 +1,62 @@
+import 'phaser';
+
+export default class Bullets extends Phaser.Physics.Arcade.Group {
+  constructor (world, scene, children) {
+    super(world, scene, children);
+    this.scene = scene;
+
+    this.createMultiple({
+      frameQuantity: 5,
+      key: 'bullet',
+      active: false,
+      visible: false
+    });
+  }
+
+  enemyCollision (bullet, enemy) {
+    bullet.active = false;
+    bullet.visible = false;
+    bullet.destroy();
+    enemy.loseHealth();
+  }
+
+  fireBullet (x, y, direction) {
+    const bullet = this.getFirstDead(false);
+    if (bullet) {
+      this.scene.physics.add.existing(bullet);
+      bullet.active = true;
+      bullet.visible = true;
+      bullet.setPosition(x, y);
+      bullet.setScale(0.1);
+
+      switch (direction) {
+        case 'up':
+          bullet.body.setVelocityY(-300);
+          break;
+        case 'down':
+          bullet.body.setVelocityY(300);
+          break;
+        case 'left':
+          bullet.body.setVelocityX(-300);
+          break;
+        case 'right':
+          bullet.body.setVelocityX(300);
+          break;
+        default:
+          bullet.body.setVelocityY(-300);
+      }
+
+      this.scene.time.addEvent({
+        delay: 1500,
+        callback: () => {
+          bullet.active = false;
+          bullet.visible = false;
+          if (bullet.body) {
+            bullet.body.setVelocity(0);
+            bullet.destroy();   
+          }
+        }
+      });
+    }
+  }
+}
